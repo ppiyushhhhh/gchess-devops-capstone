bb5e0e15ceb311d94bb155d31dbb8c3f67a98ca4
Ubuntu <ubuntu@ip-172-31-7-210.ap-south-1.compute.internal>
2026-09-25 17:07:47 +0000

    ci: fix configuration and simplify pipeline

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/spf13/viper"
@@ -20,10 +22,32 @@ var (
 )
 
 func init() {
-	viper.SetConfigName("config") // name of config flie (no extension)
+	viper.SetConfigName("config")
 	viper.SetConfigType("json")
-	viper.AddConfigPath(".infra/")
-	err := viper.ReadInConfig()
+
+	// Find the project root by walking upward until .infra/config.json exists.
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(fmt.Errorf("fatal error getting working directory: %s", err))
+	}
+
+	for {
+		configPath := filepath.Join(dir, ".infra", "config.json")
+
+		if _, err := os.Stat(configPath); err == nil {
+			viper.AddConfigPath(filepath.Join(dir, ".infra"))
+			break
+		}
+
+		parent := filepath.Dir(dir)
+		if parent == dir {
+			panic(fmt.Errorf("fatal error config file: .infra/config.json not found"))
+		}
+
+		dir = parent
+	}
+
+	err = viper.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("fatal error config file: %s", err))
 	}
@@ -40,4 +64,20 @@ func init() {
 	DBHost = viper.GetString("database.host")
 	DBUser = viper.GetString("database.user")
 	DBPassword = viper.GetString("database.password")
+
+	if value := os.Getenv("DB_NAME"); value != "" {
+		DBName = value
+	}
+
+	if value := os.Getenv("DB_HOST"); value != "" {
+		DBHost = value
+	}
+
+	if value := os.Getenv("DB_USER"); value != "" {
+		DBUser = value
+	}
+
+	if value := os.Getenv("DB_PASSWORD"); value != "" {
+		DBPassword = value
+	}
 }
